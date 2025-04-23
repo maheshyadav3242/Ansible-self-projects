23bcfcbdf07e21afea60e670d77398be01d4da4e
mahesh yadav <59454115+maheshyadav3242@users.noreply.github.com>
2025-04-23 15:34:01 +0000

    second

--- firstpython.py/firstpyhton1.py
+++ firstpython.py/firstpyhton1.py
@@ -1,22 +1,26 @@
 import boto3
 ec2 = boto3.client('ec2')
-response = ec2.describe_instances(Filters=[{'Name': 'instance-state-name', 'Values': ['running']}])
-snapshoots_response = ec2.describe_snapshots(OwnerIds=['self'])
-
-active_instances_ids = set()
+response = ec2.describe_instances(filters=[{"Name": "instance-state-name", "Values": ["running"]}])
+snapshots = ec2.describe_snapshots(OwnerIds=['self'])
+instances_ids = set()
 
 for reservation in response['Reservations']:
     for instance in reservation['Instances']:
-      
-        active_instances_ids.add(instance['InstanceId'])
-for snapshot in snapshoots_response['Snapshots']:
-    snapshot_id = snapshot['SnapshotId']
-    volume_id = snapshot.get('VolumeId')
-    print("no snapshot id")
-    if not volume_id:
-        ec2.delete_snapshot(SnapshotId=snapshot_id)
-    else:
-        volume_response = ec2.describe_volumes(VolumeIds=[volume_id])
-        if not volume_response['Volumes'][0]['Attachments']:
-            print(f"Deleting snapshot {snapshot_id} for volume {volume_id}")
-            ec2.delete_snapshot(SnapshotId=snapshot_id)
+        instances_ids.add(instance['InstanceId'])
+for snapshots in snapshots['Snapshots']:
+   snapshotid = snapshots['SnapshotId']
+   voulmeid = snapshots['VolumeId']
+   if not voulmeid:
+       ec2.delete_snapshot(SnapshotId=snapshotid)
+       print(f"Deleted snapshot {snapshotid} with volume ID {voulmeid}")
+   else:
+        try:
+            volume = ec2.describe_volumes(VolumeIds=[voulmeid])
+            if not volume['Volumes'][0]['Attachments']:
+                ec2.delete_snapshot(SnapshotId=snapshotid)
+                print(f"Deleted snapshot {snapshotid} with volume ID {voulmeid}")      
+        except Exception.clienterror as e:
+            if e.response['Error']['Code'] == 'Invalidvolume.NotFound':
+                print(f"Snapshot {snapshotid} is in use and cannot be deleted.")
+                ec2.delete_snapshot(SnapshotId=snapshotid)
+           
