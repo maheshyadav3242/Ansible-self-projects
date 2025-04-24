7041784d3a3e2e92d09ff3ec781aa39fb6aa351b
mahesh yadav <59454115+maheshyadav3242@users.noreply.github.com>
2025-04-24 07:22:51 +0000

    practise

--- firstpython.py/firstpyhton1.py
+++ firstpython.py/firstpyhton1.py
@@ -1,26 +1,28 @@
 import boto3
-ec2 = boto3.client('ec2')
-response = ec2.describe_instances(filters=[{"Name": "instance-state-name", "Values": ["running"]}])
-snapshots = ec2.describe_snapshots(OwnerIds=['self'])
-instances_ids = set()
 
+ec2 = boto3.client('ec2')
+response =ec2.describe_instances(Filters=[{'Name': 'instance-state-name', 'Values': ['running']}])
+snapshots = ec2.describe_snapshots(OwnerIds=['self'])
+instance_ids=set()
 for reservation in response['Reservations']:
     for instance in reservation['Instances']:
-        instances_ids.add(instance['InstanceId'])
-for snapshots in snapshots['Snapshots']:
-   snapshotid = snapshots['SnapshotId']
-   voulmeid = snapshots['VolumeId']
-   if not voulmeid:
-       ec2.delete_snapshot(SnapshotId=snapshotid)
-       print(f"Deleted snapshot {snapshotid} with volume ID {voulmeid}")
-   else:
+        instance_ids.add(instance['InstanceId'])
+for snapshot in snapshots['Snapshots']:
+    snapshotid =snapshot['SnapshotId']
+    volume_id = snapshot['VolumeId']
+    if not volume_id:
+        ec2.delete_snapshot(SnapshotId=snapshotid)
+        print(f"Deleted snapshot {snapshotid} for volume {volume_id}")
+    else:   
         try:
-            volume = ec2.describe_volumes(VolumeIds=[voulmeid])
+            volume = ec2.describe_volumes(VolumeIds=[volume_id])
             if not volume['Volumes'][0]['Attachments']:
                 ec2.delete_snapshot(SnapshotId=snapshotid)
-                print(f"Deleted snapshot {snapshotid} with volume ID {voulmeid}")      
-        except Exception.clienterror as e:
-            if e.response['Error']['Code'] == 'Invalidvolume.NotFound':
-                print(f"Snapshot {snapshotid} is in use and cannot be deleted.")
+        except ec2.exceptions.ClientError as e:
+            if e.response['Error']['Code'] == 'InvalidVolume.NotFound':
+                # The volume associated with the snapshot is not found (it might have been deleted)
                 ec2.delete_snapshot(SnapshotId=snapshotid)
-           
+                print(f"Deleted EBS snapshot {snapshotid} as its associated volume was not found.")
+        except Exception as e:
+            print(f"An error occurred: {e}")
+   
---
+++ firstpython.py/practise.py
@@ -0,0 +1,27 @@
+import boto3
+
+ec2 = boto3.client('ec2')
+response =ec2.describe_instances(Filters=[{'Name': 'instance-state-name', 'Values': ['running']}])
+snapshots = ec2.describe_snapshots(OwnerIds=['self'])
+instance_ids=set()
+for reservation in response['Reservations']:
+    for instance in reservation['Instances']:
+        instance_ids.add(instance['InstanceId'])
+for snapshot in snapshots['Snapshots']:
+    snapshotid =snapshot['SnapshotId']
+    volume_id = snapshot['VolumeId']
+    if not volume_id:
+        ec2.delete_snapshot(SnapshotId=snapshotid)
+        print(f"Deleted snapshot {snapshotid} for volume {volume_id}")
+    else:   
+        try:
+            volume = ec2.describe_volumes(VolumeIds=[volume_id])
+            if not volume['Volumes'][0]['Attachments']:
+                ec2.delete_snapshot(SnapshotId=snapshotid)
+        except ec2.exceptions.ClientError as e:
+            if e.response['Error']['Code'] == 'InvalidVolume.NotFound':
+                # The volume associated with the snapshot is not found (it might have been deleted)
+                ec2.delete_snapshot(SnapshotId=snapshotid)
+                print(f"Deleted EBS snapshot {snapshotid} as its associated volume was not found.")
+        except Exception as e:
+            print(f"An error occurred: {e}")
